858293d87bdabbab9960e076b2935b8d1f4689a7
David-Lazaro-Fernandez <davidlazaro20@hotmail.com>
2023-08-27 17:00:40 -0600

    Deleted Andrea's Portfolio Folder

--- andrea-portfolio/next.config.js
+++
@@ -1,9 +0,0 @@
-const { withContentlayer } = require("next-contentlayer");
-
-module.exports = withContentlayer({
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false, path: false };
-
-    return config;
-  },
-});
