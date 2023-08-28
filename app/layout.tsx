8723e7d289783417d86d30ee24d104a5304ea254
David-Lazaro-Fernandez <davidlazaro20@hotmail.com>
2023-08-27 20:39:41 -0600

    [FIX & ADDED] Fixed Modal responsiveness and added metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,30 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Andrea Arredondo',
   description: 'Andrea Arredondo personal website',
+  keywords: ['Andrea Arredondo', 'Andrea', 'Arredondo', 'Personal Website', 'Portfolio'],
+  authors: [{name: 'David Lazaro'},{name: 'Andrea Arredondo'}],
+  creator: 'David & Andrea',
+  metadataBase: new URL('https://andrea-arredondo.vercel.app/'),
+  alternates: {
+    canonical: '/',
+    languages: {
+      'en-US': '/en-US',
+    },
+  },
+  openGraph: {
+    title: 'Andrea Arredondo',
+    description: 'Andrea Arredondo personal website',
+    url: 'https://andrea-arredondo.vercel.app/',
+    siteName: 'Andrea Arredondo',
+    type: 'website',	
+    images: [
+      {
+        url: 'https://npr.brightspotcdn.com/dims4/default/6d84553/2147483647/strip/true/crop/1168x613+24+0/resize/1200x630!/quality/90/?url=https%3A%2F%2Fmedia.npr.org%2Fassets%2Fimg%2F2023%2F01%2F14%2Fthis-is-fine_custom-dcb93e90c4e1548ffb16978a5a8d182270c872a9.jpg',
+        width: 1200,
+        height: 630,
+      },
+    ]
+  }
 }
 
 export default function RootLayout({
